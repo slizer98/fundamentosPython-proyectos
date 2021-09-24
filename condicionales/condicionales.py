17c3bf344a5aed44e74699b758ae2011a6a4e996
slizer98 <erick.m.villa.r@gmail.com>
2021-09-24 08:26:27 -0500

    agregando archivos en los proyectos

--- condicionales/condicionales.py
+++ condicionales/condicionales.py
@@ -4,6 +4,7 @@
 #     print("ya eres mayor de edad !")
 # else: 
 #     print("Eres menor de edad !!!!")
+
 numero = int(input("Escribe un numero: "))
 if numero > 5:
     print('Es mayor a 5')
@@ -11,3 +12,4 @@ elif numero == 5:
     print('Es igual a 5')
 else:
     print('Es menor a 5')
+
